86c4d738ea832a4e3b70e57f07543c0571b2a36b
Christoph Egger <egger@cs.fau.de>
2019-02-13 16:33:46 +0100

    Add config for PoPETs

--- config.py
+++ config.py
@@ -199,6 +199,11 @@ add_journal("TCHES", 2018, "tches", "Transactions on Cryptographic Hardware and 
             months=[],
             url="httpss://dblp.uni-trier.de/db/journals/tches/tches${year}.html")
 
+add_journal("POPETS", 2015, "popets", "Proceedings on Privacy Enhancing Technologies",
+            months=["jan", "apr", "jul", "oct"],
+            url="https://dblp.uni-trier.de/db/journals/popets/popets${year}.html",
+            publisher="degruyter")
+
 add_misc("EPRINT", "Cryptology ePrint Archive", url="https://eprint.iacr.org/${year}")
 
 
